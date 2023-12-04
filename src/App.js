86ae9392c38feb0818b412ac9fc64ac1c5135187
AndrzejNowakDev <andrzej.nowak.dev@gmail.com>
2023-12-04 07:26:06 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { Editor, EditorState } from 'draft-js'
+import { Editor, EditorState, KeyBindingUtil } from 'draft-js'
 import 'draft-js/dist/Draft.css'
 import { useState } from 'react';
 import { RichUtils } from 'draft-js';
 
 function App() {
+  const styleMap = {
+    'HIGHLIGHT': {
+      'backgroundColor': '#faed27',
+    }
+  };
+  
   const [editorState, setEditorState] = useState(
     () => EditorState.createEmpty(),
   )
@@ -18,11 +24,35 @@ function App() {
     setEditorState(RichUtils.toggleInlineStyle(editorState, 'ITALIC'))
   }
 
+  const onHighlightClick = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, 'HIGHLIGHT'))
+  }
+
+  const handleKeyCommand = (command) => {
+    if (!editorState && command === 'highlight') {
+      editorState = RichUtils.toggleInlineStyle(this.state.editorState, 'HIGHLIGHT');
+    }
+  } 
+
+  const keyBindingFunction = (event) => {
+    if (KeyBindingUtil.hasCommandModifier(event) && event.shiftKey && event.key === 'h') {
+      return 'highlight';
+    }
+  }
+
   return (
     <div className="App">
        <button onClick={onBoldClick}>Bold</button>
        <button onClick={onItalicClick}>Italic</button>
-      <Editor editorState={editorState} onChange={setEditorState} />
+       <button onClick={onHighlightClick}>HIGHLIGHT</button>
+      <Editor
+        placeholder={"Start typing!"}
+        customStyleMap={styleMap} 
+        editorState={editorState} 
+        onChange={setEditorState}
+        handleKeyCommand={handleKeyCommand}
+        keyBindingFn={keyBindingFunction} 
+      />
     </div>
   );
 }
