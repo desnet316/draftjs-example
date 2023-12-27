1a4456d09cc934793ba9c8bd421b719efd98614c
AlexCPT-cpu <alexodey79@gmail.com>
2023-12-27 13:34:53 +0100

    xx

--- src/helpers/mapIDsToContent.js
+++ src/helpers/mapIDsToContent.js
@@ -2,18 +2,19 @@ export default function mapIDsToContent(initialContent, highlightIDs, uuidv4) {
   const mappedContent = { ...initialContent };
 
   mappedContent.blocks.forEach((block) => {
-    const isAllHighlighted = block.text.split(/\s+/).every((word) => {
-      const styleRange = block.inlineStyleRanges.find(
-        (s) =>
-          s.offset === 0 && s.length === word.length && s.style === "HIGHLIGHT"
+    const blockTextLength = block.text.length;
+
+    const allTextHighlighted = block.inlineStyleRanges.every((styleRange) => {
+      return (
+        styleRange.style === "HIGHLIGHT" &&
+        styleRange.length === blockTextLength
       );
-      return !!styleRange;
     });
 
-    if (isAllHighlighted) {
+    if (allTextHighlighted) {
       const existingId = highlightIDs.find((id) => id.id);
       const idToAssign = existingId ? existingId.id : uuidv4();
-      console.log(existingId);
+
       block.inlineStyleRanges.forEach((styleRange) => {
         if (styleRange.style === "HIGHLIGHT") {
           styleRange.id = idToAssign;
@@ -26,13 +27,24 @@ export default function mapIDsToContent(initialContent, highlightIDs, uuidv4) {
             (id) =>
               id.blockKey === block.key && id.styleOffset === styleRange.offset
           );
+
           if (!id || !id.id) {
-            styleRange.id = uuidv4();
-            highlightIDs.push({
-              id: styleRange.id,
-              blockKey: block.key,
-              styleOffset: styleRange.offset,
-            });
+            if (block.inlineStyleRanges.some((s) => s.style === "HIGHLIGHT")) {
+              const existingId = highlightIDs.find(
+                (id) =>
+                  id.blockKey === block.key &&
+                  id.styleOffset === styleRange.offset
+              );
+
+              styleRange.id = existingId ? existingId.id : uuidv4();
+            } else {
+              styleRange.id = uuidv4();
+              highlightIDs.push({
+                id: styleRange.id,
+                blockKey: block.key,
+                styleOffset: styleRange.offset,
+              });
+            }
           } else {
             styleRange.id = uuidv4();
             highlightIDs.push({
