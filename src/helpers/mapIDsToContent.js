3c4ab28bf4592e5d180cb831f0cff775bd4405d8
AlexCPT-cpu <alexodey79@gmail.com>
2023-12-27 11:28:47 +0100

    updated id fields

--- src/helpers/mapIDsToContent.js
+++ src/helpers/mapIDsToContent.js
@@ -1,17 +1,41 @@
-export default function mapIDsToContent(initialContent, highlightIDs) {
+export default function mapIDsToContent(initialContent, highlightIDs, uuidv4) {
   const mappedContent = { ...initialContent };
+  const highlightedBlocks = mappedContent.blocks.filter((block) =>
+    block.inlineStyleRanges.some((s) => s.style === "HIGHLIGHT")
+  );
 
-  highlightIDs.forEach((id) => {
-    const block = mappedContent.blocks.find((b) => b.key === id.blockKey);
-    if (block) {
-      const styleRange = block.inlineStyleRanges.find(
-        (s) => s.offset === id.styleOffset
-      );
-      if (styleRange) {
-        styleRange.id = id.id;
+  if (highlightedBlocks.length === mappedContent.blocks.length) {
+    const existingId = highlightIDs.find((id) => id.id);
+    const idToAssign = existingId ? existingId.id : uuidv4();
+
+    mappedContent.blocks.forEach((block) => {
+      block.inlineStyleRanges.forEach((styleRange) => {
+        if (styleRange.style === "HIGHLIGHT") {
+          styleRange.id = idToAssign;
+        }
+      });
+    });
+  } else {
+    highlightIDs.forEach((id) => {
+      const block = mappedContent.blocks.find((b) => b.key === id.blockKey);
+      if (block) {
+        const styleRangeIndex = block.inlineStyleRanges.findIndex(
+          (s) => s.offset === id.styleOffset && s.style === "HIGHLIGHT"
+        );
+        if (styleRangeIndex !== -1) {
+          const styleRange = block.inlineStyleRanges[styleRangeIndex];
+          styleRange.id = id.id;
+        } else {
+          const styleRange = block.inlineStyleRanges.find(
+            (s) => s.offset === id.styleOffset
+          );
+          if (styleRange && styleRange.id) {
+            styleRange.id = null;
+          }
+        }
       }
-    }
-  });
+    });
+  }
 
   return mappedContent;
 }
--- src/helpers/modifyContent.js
+++ src/helpers/modifyContent.js
@@ -1,23 +1,54 @@
-export default function modifyContentWithIdPreservation(contentData, uuidv4) {
-  // Get the initial content
+export default function modifyContentWithIdPreservation(
+  contentData,
+  uuidv4,
+  highlightIDs
+) {
   const content = contentData;
-  const id = uuidv4();
-  // Modify the content while preserving inline style IDs
+
   const modifiedContent = {
     ...content,
     blocks: content.blocks.map((block) => {
+      const index = highlightIDs.findIndex((id) => id.blockKey === block.key);
+
+      let id = null;
+      if (index !== -1) {
+        id = highlightIDs[index].id;
+      } else {
+        id = uuidv4();
+        highlightIDs.push({ id, blockKey: block.key, styleOffset: -1 });
+      }
+
+      const isAllHighlighted = block.inlineStyleRanges.every(
+        (styleRange) => styleRange.style === "HIGHLIGHT"
+      );
+
       const modifiedInlineStyleRanges = block.inlineStyleRanges.map(
         (styleRange) => {
-          // Here, you can preserve and use the ID associated with the 'HIGHLIGHT' style
-          if (styleRange.style === "HIGHLIGHT" /*&& styleRange.id*/) {
-            // Your logic to use the ID when modifying the style
-            // For example, update the text or offset
+          if (isAllHighlighted && styleRange.style === "HIGHLIGHT") {
             return {
               ...styleRange,
-              id: id, // Modify the offset as an example
+              id: id,
             };
+          } else if (styleRange.style === "HIGHLIGHT" && !styleRange.id) {
+            const newId = uuidv4();
+            styleRange.id = newId;
+            highlightIDs.push({
+              id: newId,
+              blockKey: block.key,
+              styleOffset: styleRange.offset,
+            });
+            return {
+              ...styleRange,
+              id: newId,
+            };
+          } else if (!styleRange.id && id) {
+            return {
+              ...styleRange,
+              id: id,
+            };
+          } else {
+            return styleRange;
           }
-          return styleRange;
         }
       );
 
